14831a056c9cb4edd9c4d577dfd448d2dc4ccc56
Cody Abbott <cody.abbott16@gmail.com>
2020-04-20 17:04:30 +0100

    css for home and about with cards added

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,48 @@
 import React from "react";
+import "../about.css";
+import portrait1 from "../imgs/butcher_portrait_1.jpg";
+import portrait2 from "../imgs/butcher_portrait_2.jpg";
+import portrait3 from "../imgs/butcher_portrait_3.jpg";
 
 export default function About() {
   return (
-    <div>
-      <h1>about</h1>
-    </div>
+    <>
+      <h1 className="team-header">Meet the Team!</h1>
+      <div className="card-container">
+        <div class="card">
+          <img src={portrait1} alt="John" />
+          <h1>John Doe</h1>
+          <p class="title">Proud Father & Local Butcher</p>
+          <p>20 Years Experience</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            suscipit ultrices tristique. Cras placerat posuere vehicula. Aliquam
+            eget dignissim quam, sed tempus massa.
+          </p>
+        </div>
+        <div class="card">
+          <img src={portrait2} alt="Jane" />
+          <h1>Jane Doe</h1>
+          <p class="title">Proud Daugther & Local Butcher</p>
+          <p>1 Year Experience</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            suscipit ultrices tristique. Cras placerat posuere vehicula. Aliquam
+            eget dignissim quam, sed tempus massa.
+          </p>
+        </div>
+        <div class="card">
+          <img src={portrait3} alt="John" />
+          <h1>Jim Smith</h1>
+          <p class="title">Proud Friend & Local Butcher</p>
+          <p>5 Years Experience</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            suscipit ultrices tristique. Cras placerat posuere vehicula. Aliquam
+            eget dignissim quam, sed tempus massa.
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,73 @@ export default function Home() {
   return (
     <>
       <div className="hero-image">
-        <h1 className="business-heading">Local High Street Butcher</h1>
+        <h1 className="business-heading">
+          Local <br />
+          High <br />
+          Street <br />
+          Butcher
+        </h1>
       </div>
-      <div>
-        <h2>Info goes here</h2>
+      <div className="home-information">
+        <p className="opening-info">
+          <span id="intro-para">Hello...</span>
+          <br /> <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec sem
+          urna. Fusce mi neque, pellentesque eu erat et, consequat pharetra
+          nulla. Maecenas felis nisl, sagittis non dui non, scelerisque
+          condimentum libero. Suspendisse laoreet, neque volutpat maximus
+          porttitor, eros magna laoreet quam, at condimentum purus risus sed
+          sapien. Mauris vitae lectus metus. Duis dictum convallis nisl, eu
+          mattis ante facilisis at. Suspendisse sit amet lacus sapien. Praesent
+          eu pulvinar nulla, id iaculis elit.
+          <br /> <br />
+          Maecenas id lorem eget est venenatis sollicitudin. Fusce auctor erat
+          id ex scelerisque, sed mollis sapien euismod. Mauris consequat dapibus
+          tellus. Donec in congue sapien. Sed quis lobortis justo, ut pretium
+          tellus. Duis in scelerisque lorem. Vestibulum vel tellus massa. Proin
+          sagittis pellentesque quam et porta. Sed non lacus auctor, faucibus
+          nulla quis, fermentum metus. Maecenas mollis, mi eu lobortis luctus,
+          lorem ante rhoncus tortor, blandit aliquam dui felis ut enim.
+          Suspendisse elementum et felis ut ultrices.
+        </p>
+        <table className="opening-times">
+          <tr>
+            <th colspan="2">Opening Times</th>
+          </tr>
+          <tr>
+            <td>Monday</td>
+            <td>9am - 5pm</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>9am - 5pm</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>9am - 5pm</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>9am - 5pm</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>9am - 3pm</td>
+          </tr>
+        </table>
+        <table className="contact-info">
+          <tr>
+            <th colspan="2">Contact Info</th>
+          </tr>
+          <tr>
+            <td>Telephone: </td>
+            <td>0131 *** ****</td>
+          </tr>
+          <tr>
+            <td>Email: </td>
+            <td>youremail@business.com</td>
+          </tr>
+        </table>
       </div>
     </>
   );
