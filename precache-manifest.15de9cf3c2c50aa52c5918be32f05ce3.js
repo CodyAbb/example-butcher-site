20e3067a3d0ce0ca1744fda3c4cf63285b2a63ea
Cody Abbott <cody.abbott16@gmail.com>
2020-04-27 12:23:06 +0100

    Updates

---
+++ precache-manifest.15de9cf3c2c50aa52c5918be32f05ce3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6866d158658633d05ac5466718a3731f",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/index.html"
+  },
+  {
+    "revision": "78a09589349a85b84f24",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/css/main.d435180b.chunk.css"
+  },
+  {
+    "revision": "60b3f308b004dee51bfa",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/js/2.fe7b2f54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/js/2.fe7b2f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a09589349a85b84f24",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/js/main.cc1dc373.chunk.js"
+  },
+  {
+    "revision": "fed16a4d4abd5414d756",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/js/runtime-main.7c7919af.js"
+  },
+  {
+    "revision": "98db1f3723c14ba04b9650fe6aeadd10",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/media/butcher_photo.98db1f37.jpg"
+  },
+  {
+    "revision": "0484f9026dd8605a0149eb3e55768c38",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/media/butcher_portrait_1.0484f902.jpg"
+  },
+  {
+    "revision": "fc5eb0a6da81eb38291d7f0c138c7449",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/media/butcher_portrait_2.fc5eb0a6.jpg"
+  },
+  {
+    "revision": "95389a143bc5aa46521a0a4b0fa9e320",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/media/butcher_portrait_3.95389a14.jpg"
+  },
+  {
+    "revision": "8a24ecfee3c59a5d32d190531dcb9f75",
+    "url": "/http//CodyAbb.github.io/example-butcher-site/static/media/meat_product.8a24ecfe.jpg"
+  }
+]);
